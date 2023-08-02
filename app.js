9ebe2f44526e3beba52908d99b5d5dd8dfb9a6ca
Josseth Rodriguez <hfernando382@gmail.com>
2023-08-02 12:09:29 -0400

    build: creating interactive function in js

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ let your_score = 0
 let opponent_score = 0
 
 let choices = ['rock', 'papper', 'scissors']
+
+window.onload = function() {
+    for (let i = 0; i < 3; i++) {
+        let choice = document.createElement('img')
+        choice.id = choices[i]
+        choices.src = choices[i] + '.png'
+        document.getElementById('choices').append(choice)
+    }
+}
